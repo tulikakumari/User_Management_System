493ca2bafe3dcb4b63590ea0750e3a56e9cb6791
Tulika Kumari <tulikakashyap1999@gmail.com>
2021-04-04 18:46:27 +0530

    User Management System using crud operations

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyparser = require("body-parser");
+const path = require('path');
+const app = express();
+
+const connectDB = require('./server/database/connection');
+
+dotenv.config({path:'config.env'});
+
+const PORT = process.env.PORT||8080
+
+// log request
+
+app.use(morgan('tiny'));
+
+// mongoDb connection
+connectDB();
+
+// parse request to body parser
+
+app.use(bodyparser.urlencoded({extended:true}));
+
+// set view engine
+
+app.set("view engine","ejs")
+// app.set('views',path.resolve(__dirname,"views/ejs"))
+
+
+// load asset using middleware
+
+app.use('/css',express.static(path.resolve(__dirname,'assets/css')))
+app.use('/img',express.static(path.resolve(__dirname,'assets/img')))
+app.use('/js',express.static(path.resolve(__dirname,'assets/js')))
+
+// load routers
+app.use('/',require('./server/routes/router'))
+
+app.listen(PORT,() =>
+{
+    console.log(`Server is running on http://localhost:${PORT}`)
+});
---
+++ server/database/connection.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+// const MONGO_URI="mongodb+srv://admin:admin@123@cluster0.bvvhv.mongodb.net/users?retryWrites=true&w=majority"
+
+const connectDB = async() => {
+    try{
+        // mongodb connection string
+        const con = await mongoose.connect(process.env.MONGO_URI,{
+            useNewUrlParser:true,
+            useUnifiedTopology:true,
+            useFindAndModify:false,
+            useCreateIndex:true
+        });
+
+        console.log(`MongoDb connected : ${con.connection.host}`)
+    }catch(err)
+    {
+        console.log(err);
+        process.exit(1);
+    }
+} 
+
+module.exports = connectDB;
