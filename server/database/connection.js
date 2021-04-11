a4c461702e20b2081b9455adb9dd91e1ef4175d4
Tulika Kumari <tulikakashyap1999@gmail.com>
2021-04-11 10:58:59 +0530

    User Management using Crud operations

--- server/database/connection.js
+++ server/database/connection.js
@@ -2,13 +2,15 @@ const mongoose = require('mongoose');
 
 const connectDB = async() => {
     try{
+        // added consts to fix commit
+        //const MONGO_URI
         // mongodb connection string
-        const con = await mongoose.connect(process.env.MONGO_URI,{
-            useNewUrlParser:true,
-            useUnifiedTopology:true,
-            useFindAndModify:false,
-            useCreateIndex:true
-        });
+        // const con = await mongoose.connect(process.env.MONGO_URI,{
+        //     useNewUrlParser:true,
+        //     useUnifiedTopology:true,
+        //     useFindAndModify:false,
+        //     useCreateIndex:true
+        // });
 
         console.log(`MongoDb connected : ${con.connection.host}`)
     }catch(err)
